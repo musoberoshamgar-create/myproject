ba7b1c6d73d789cac6944c54767e225be2f3ed80
musoberoshamgar-create <musoberoshamgar@gmail.com>
2026-08-09 12:47:09 +0200

    Update docker.py

--- tabbycat/settings/docker.py
+++ tabbycat/settings/docker.py
@@ -9,10 +9,10 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'tabbycat'),
-        'USER': os.environ.get('POSTGRES_USER', 'tabbycat'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'NAME': os.environ.get('POSTGRES_DB', 'tabbycat_2w63'),
+        'USER': os.environ.get('POSTGRES_USER', 'tabbycat_2w63_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'vDH1ZvccgVrqhjbaJ5YbDQh8wOCbnEbn'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'dpg-d9s5ifn40ujc73clc0ug-a'),
         'PORT': int(os.environ.get('POSTGRES_PORT', '5432')),
     }
 }
