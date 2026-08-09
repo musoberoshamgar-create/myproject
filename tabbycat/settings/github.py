7ed9679dcb80ee7d43502459afdcd6e88f5ccdf9
musoberoshamgar-create <musoberoshamgar@gmail.com>
2026-08-09 13:10:26 +0200

    Update github.py

--- tabbycat/settings/github.py
+++ tabbycat/settings/github.py
@@ -5,10 +5,11 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'postgres',
-        'NAME': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
+        'NAME': os.environ.get('POSTGRES_DB', 'tabbycat_2w63'),
+        'USER': os.environ.get('POSTGRES_USER', 'tabbycat_2w63_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'vDH1ZvccgVrqhjbaJ5YbDQh8wOCbnEbn'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'dpg-d9s5ifn40ujc73clc0ug-a'),
+        'PORT': int(os.environ.get('POSTGRES_PORT', '5432')),
+    }
 }
+
